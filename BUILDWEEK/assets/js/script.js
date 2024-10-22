ef3412e5c90e482ba7c101dcc3f089508973ac4d
notdav04 <davide.alonzi26@gmail.com>
2024-10-22 14:51:46 +0200

    fixed checkbox css and redirect to next page in js

--- BUILDWEEK/assets/js/script.js
+++ BUILDWEEK/assets/js/script.js
@@ -16,7 +16,8 @@ let counter = 0;
 proceed_button.onclick = function () {
   if (checkbox.checked == true) {
     //se la checkbox è spuntata manda al benchmark
-    window.location.href = "benchmark.html";
+    const url = "benchmark.html";
+    window.location.href = url;
   } else {
     //se la checkbox non è spuntata mostra un alert
     if (counter === 0) {

455223a739379c87cf87f9b99212a2dce917628b
Edoardo <edfl.tunzi@gmail.com>
2024-10-23 10:07:52 +0200

    Started creation of JS file

---
+++ BUILDWEEK/assets/js/benchmark.js
@@ -0,0 +1,21 @@
+const questions = [
+  {
+    type: "multiple",
+    difficulty: "easy",
+    category: "Science: Computers",
+    question: "In web design, what does CSS stand for?",
+    correct_answer: "Cascading Style Sheet",
+    incorrect_answers: ["Counter Strike: Source", "Corrective Style Sheet", "Computer Style Sheet"],
+  },
+  {
+    type: "multiple",
+    difficulty: "easy",
+    category: "Science: Computers",
+    question: "How many values can a single byte represent?",
+    correct_answer: "256",
+    incorrect_answers: ["8", "1"],
+  },
+];
+const correctAnswers = 0;
+const wrongAnswers = 0;
+//funzione displayQuiz() per mostrare question e risposte nell'html

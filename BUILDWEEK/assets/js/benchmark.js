b54c642e2a40f6377a16a597b5e6a92141abddf2
Edoardo <edfl.tunzi@gmail.com>
2024-10-23 12:50:12 +0200

    JS benchmark  array of questions added

--- BUILDWEEK/assets/js/benchmark.js
+++ BUILDWEEK/assets/js/benchmark.js
@@ -1,4 +1,4 @@
-const questions = [
+/*const arrayQuestions = [
   {
     type: "multiple",
     difficulty: "easy",
@@ -18,4 +18,113 @@ const questions = [
 ];
 const correctAnswers = 0;
 const wrongAnswers = 0;
+const currentQuestionindex = 0;
+
+//funzione shuffleAnswers, per mescolare l'ordine delle risposte
+const shuffleAnswers = (currentQuestionindex) => {
+  let allAnswers = [arrayQuestions[currentQuestionindex].correct_answer];
+  arrayQuestions[0].incorrect_answers.forEach((answer) => allAnswers.push(answer));
+  return allAnswers.sort(() => Math.random() - 0.5);
+};
+
 //funzione displayQuiz() per mostrare question e risposte nell'html
+
+const displayQuiz = (array) => {
+  const questionTitle = document.querySelector(".question-title");
+  questionTitle.innerHTML = array[currentQuestionindex].question;
+
+
+
+  currentQuestionindex++;
+};
+
+console.log(shuffleAnswers(currentQuestionindex));
+console.log(displayQuiz(arrayQuestions));
+*/
+
+const questions = [
+  {
+    type: "boolean",
+    difficulty: "easy",
+    category: "Science: Computers",
+    question: "Time on Computers is measured via the EPOX System.",
+    correct_answer: "False",
+    incorrect_answers: ["True"],
+  },
+  {
+    type: "multiple",
+    difficulty: "easy",
+    category: "Science: Computers",
+    question: "Which company was established on April 1st, 1976 by Steve Jobs, Steve Wozniak and Ronald Wayne?",
+    correct_answer: "Apple",
+    incorrect_answers: ["Microsoft", "Atari", "Commodore"],
+  },
+  {
+    type: "multiple",
+    difficulty: "easy",
+    category: "Science: Computers",
+    question: "Which computer language would you associate Django framework with?",
+    correct_answer: "Python",
+    incorrect_answers: ["C#", "C++", "Java"],
+  },
+  {
+    type: "multiple",
+    difficulty: "easy",
+    category: "Science: Computers",
+    question: "Which computer hardware device provides an interface for all other connected devices to communicate?",
+    correct_answer: "Motherboard",
+    incorrect_answers: ["Central Processing Unit", "Hard Disk Drive", "Random Access Memory"],
+  },
+  {
+    type: "multiple",
+    difficulty: "easy",
+    category: "Science: Computers",
+    question: "How long is an IPv6 address?",
+    correct_answer: "128 bits",
+    incorrect_answers: ["32 bits", "64 bits", "128 bytes"],
+  },
+  {
+    type: "boolean",
+    difficulty: "easy",
+    category: "Science: Computers",
+    question: "The logo for Snapchat is a Bell.",
+    correct_answer: "False",
+    incorrect_answers: ["True"],
+  },
+  {
+    type: "multiple",
+    difficulty: "easy",
+    category: "Science: Computers",
+    question: "In the programming language Java, which of these keywords would you put on a variable to make sure it doesn't get modified?",
+    correct_answer: "Final",
+    incorrect_answers: ["Static", "Private", "Public"],
+  },
+  {
+    type: "boolean",
+    difficulty: "easy",
+    category: "Science: Computers",
+    question: "Pointers were not used in the original C programming language; they were added later on in C++.",
+    correct_answer: "False",
+    incorrect_answers: ["True"],
+  },
+  {
+    type: "boolean",
+    difficulty: "easy",
+    category: "Science: Computers",
+    question: "Ada Lovelace is often considered the first computer programmer.",
+    correct_answer: "True",
+    incorrect_answers: ["False"],
+  },
+  {
+    type: "multiple",
+    difficulty: "easy",
+    category: "Science: Computers",
+    question: "The numbering system with a radix of 16 is more commonly referred to as",
+    correct_answer: "Hexidecimal",
+    incorrect_answers: ["Binary", "Duodecimal", "Octal"],
+  },
+];
+
+const correctAnswers = 0;
+const wrongAnswers = 0;
+const currentQuestionindex = 0;
